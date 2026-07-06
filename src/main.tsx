3126f62c605d29f30d5238b617b60f1f9ef65085
Jedrzej Zawojski <jedrzej.zawojski95@gmail.com>
2026-07-06 23:33:57 +0200

    feat: add PostHog - activity tracking on my portfolio

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { PostHogProvider } from '@posthog/react'
 import './index.css'
 import App from './App.tsx'
 
+const options = {
+  api_host: import.meta.env.VITE_POSTHOG_HOST,
+  defaults: '2026-05-30',
+} as const
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PostHogProvider apiKey={import.meta.env.VITE_POSTHOG_PROJECT_TOKEN} options={options}>
+      <App />
+    </PostHogProvider>
   </StrictMode>,
 )
